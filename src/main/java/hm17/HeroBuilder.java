49748d571715d44b650621e19ea0ac1d28bcc273
BlondGT <112488079+BlondGT@users.noreply.github.com>
2023-04-21 23:36:19 +0300

    Changed сдфии in hm17 (Lombok)

--- src/main/java/hm17/HeroBuilder.java
+++ src/main/java/hm17/HeroBuilder.java
@@ -6,14 +6,14 @@ import lombok.*;
 @Builder
 public class HeroBuilder {
 
-    private final String name;
-    private final String gender;
-    private final String eyeColor;
-    private final String race;
-    private final String hairColor;
-    private final double height;
-    private final String publisher;
-    private final String skinColor;
-    private final String alignment;
-    private final int weight;
+    private String name;
+    private String gender;
+    private String eyeColor;
+    private String race;
+    private String hairColor;
+    private double height;
+    private String publisher;
+    private String skinColor;
+    private String alignment;
+    private int weight;
 }
