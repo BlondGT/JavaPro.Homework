f4f65cf69fe8d60df4d8e19fcc380f77dce2b254
BlondGT <112488079+BlondGT@users.noreply.github.com>
2023-04-23 22:36:25 +0300

    Added hm21 (JDBC) + changed build.gradle

---
+++ src/main/java/hm21/HeroDao.java
@@ -0,0 +1,12 @@
+package hm21;
+
+import java.util.List;
+
+public interface HeroDao {
+
+    List<Hero> findAll();
+    List<Hero> findByName(String name);
+    void create(Hero hero);
+    void update(Hero hero);
+    boolean delete(Long id);
+}
