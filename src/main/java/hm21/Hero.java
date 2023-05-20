4a67fe017824d68f25d5e8c3f3e62cfe1eb66daf
Tatiana <112488079+BlondGT@users.noreply.github.com>
2023-05-20 10:18:48 +0300

    Changed REST
    * Added hm24 (REST) and changed some classes
    
    * Fixed hm24 (REST)
    
    * Fixed hm 24

--- src/main/java/hm21/Hero.java
+++ src/main/java/hm21/Hero.java
@@ -1,11 +1,15 @@
 package hm21;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 
 @Data
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class Hero {
 
     private Long id;
