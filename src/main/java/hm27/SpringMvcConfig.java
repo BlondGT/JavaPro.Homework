3ba136d722f1ada3570cf44fab0094243188c838
BlondGT <112488079+BlondGT@users.noreply.github.com>
2023-05-24 22:03:07 +0300

    Added hm27 (Spring MVC)

---
+++ src/main/java/hm27/SpringMvcConfig.java
@@ -0,0 +1,28 @@
+package hm27;
+
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+import hm21.HeroDao;
+import hm21.HeroDaoImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class SpringMvcConfig {
+
+    @Bean
+    public HeroDao heroDao() {
+        return new HeroDaoImpl(hikariDataSource());
+    }
+
+    @Bean
+    private static DataSource hikariDataSource() {
+        var config = new HikariConfig();
+        config.setJdbcUrl("jdbc:postgresql://localhost:5432/postgres");
+        config.setUsername("postgres");
+        config.setPassword("postgres");
+        return new HikariDataSource(config);
+    }
+}
---
+++ src/main/java/hm27/SpringMvcController.java
@@ -0,0 +1,48 @@
+package hm27;
+
+import hm21.Hero;
+import hm21.HeroDao;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/")
+@RequiredArgsConstructor
+public class SpringMvcController {
+
+    private final HeroDao heroDao;
+
+    @GetMapping
+    public String heroes(Model model) {
+        model.addAttribute("name", "Visitors");
+        model.addAttribute("heroes", heroDao.findAll());
+        return "heroes/index";
+    }
+
+    @GetMapping("/{id}")
+    public String hero(Model model, @PathVariable("id") Long id) {
+        Hero hero = heroDao.findById(id);
+        if (hero == null) {
+            throw new HeroNotFoundException("Hero not found with ID: " + id);
+        }
+        model.addAttribute("hero", hero);
+        return "heroes/hero";
+    }
+
+    @GetMapping("/create")
+    public String create(Model model) {
+        model.addAttribute("hero", Hero.builder().build());
+        return "heroes/create";
+    }
+
+    @PostMapping("/add")
+    public String create(Hero hero) {
+        heroDao.create(hero);
+        return "redirect:/index";
+    }
+}
