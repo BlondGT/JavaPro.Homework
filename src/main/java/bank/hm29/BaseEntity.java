2597692bb526f6689cb1b0187be185964bc884e7
BlondGT <112488079+BlondGT@users.noreply.github.com>
2023-06-07 21:52:14 +0300

    Fixed hm29(DB Versioning + Bank)

--- src/main/java/bank/hm29/BaseEntity.java
+++ src/main/java/bank/hm29/BaseEntity.java
@@ -1,19 +1,18 @@
 package bank.hm29;
 
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.MappedSuperclass;
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.Instant;
 
 @Getter
 @Setter
 @MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
 public class BaseEntity {
 
     @Id
