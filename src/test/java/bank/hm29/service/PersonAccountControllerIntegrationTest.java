5bf8737948d730f46ff7fdccecc7d5cf606a3177
BlondGT <112488079+BlondGT@users.noreply.github.com>
2023-06-13 20:48:41 +0300

    Fixed test hm30 (Integration tests)

--- src/test/java/bank/hm29/service/PersonAccountControllerIntegrationTest.java
+++ src/test/java/bank/hm29/service/PersonAccountControllerIntegrationTest.java
@@ -11,6 +11,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -36,8 +37,115 @@ class PersonAccountControllerIntegrationTest {
     @Autowired
     protected MockMvc mockMvc;
 
+    @Autowired
+    protected ObjectMapper objectMapper;
 
     @Test
+    @DirtiesContext
+    void shouldGetAllAccounts() throws Exception {
+
+        personRepository.save(Person.builder()
+                .name("Anna Lex")
+                .email("annalex@gmail.com")
+                .uid("9654")
+                .build());
+
+        personRepository.save(Person.builder()
+                .name("Tim Tent")
+                .email("timtent@gmail.com")
+                .uid("1098")
+                .build());
+
+        personRepository.save(Person.builder()
+                .name("Joe More")
+                .email("joemorex@gmail.com")
+                .uid("2345")
+                .build());
+
+        accountRepository.save(Account.builder()
+                .uid("uuid1")
+                .iban("UA000000000000000000000123456789")
+                .balance(4500.00)
+                .person(personRepository.getReferenceById(1L))
+                .build());
+        accountRepository.save(Account.builder()
+                .uid("uuid2")
+                .iban("UA000000000000000000000987654321")
+                .balance(2700.00)
+                .person(personRepository.getReferenceById(2L))
+                .build());
+        accountRepository.save(Account.builder()
+                .uid("uuid3")
+                .iban("UA000000000000000000000918273645")
+                .balance(9000.00)
+                .person(personRepository.getReferenceById(3L))
+                .build());
+        accountRepository.save(Account.builder()
+                .uid("uuid4")
+                .iban("UA000000000000000000000123456789")
+                .balance(12000.00)
+                .person(personRepository.getReferenceById(2L))
+                .build());
+
+        mockMvc.perform(get("/api/accounts"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$", hasSize(4)));
+    }
+
+    @Test
+    @DirtiesContext
+    void shouldGetAccountsByPerson() throws Exception {
+
+        personRepository.save(Person.builder()
+                .name("Anna Lex")
+                .email("annalex@gmail.com")
+                .uid("9654")
+                .build());
+
+        personRepository.save(Person.builder()
+                .name("Tim Tent")
+                .email("timtent@gmail.com")
+                .uid("1098")
+                .build());
+
+        personRepository.save(Person.builder()
+                .name("Joe More")
+                .email("joemorex@gmail.com")
+                .uid("2345")
+                .build());
+
+        accountRepository.save(Account.builder()
+                .uid("uuid1")
+                .iban("UA000000000000000000000123456789")
+                .balance(4500.00)
+                .person(personRepository.getReferenceById(1L))
+                .build());
+        accountRepository.save(Account.builder()
+                .uid("uuid2")
+                .iban("UA000000000000000000000987654321")
+                .balance(2700.00)
+                .person(personRepository.getReferenceById(2L))
+                .build());
+        accountRepository.save(Account.builder()
+                .uid("uuid3")
+                .iban("UA000000000000000000000918273645")
+                .balance(9000.00)
+                .person(personRepository.getReferenceById(3L))
+                .build());
+        accountRepository.save(Account.builder()
+                .uid("uuid4")
+                .iban("UA000000000000000000000123456789")
+                .balance(12000.00)
+                .person(personRepository.getReferenceById(2L))
+                .build());
+
+        mockMvc.perform(get("/api/persons/{personId}/accounts", "1098"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$", hasSize(2)));
+    }
+
+    @Test
+    @DirtiesContext
     void shouldCreateAccount() throws Exception {
 
         personRepository.save(Person.builder()
@@ -83,94 +191,148 @@ class PersonAccountControllerIntegrationTest {
                 .person(personRepository.getReferenceById(2L))
                 .build());
 
-        var person = personRepository.findPersonByUid("1098").orElseThrow();
-        var uid = "uuid5";
-        var savedAccount = Account.builder()
-                .uid(uid)
+        var requestAccount = AccountDto.builder()
                 .iban("UA00000000000000000000076548932")
                 .balance(12500.00)
-                .person(person)
                 .build();
 
-        mockMvc.perform(post("/api/persons/{personId}/accounts", "1098")
-                .content(asJsonString(mapAccount(accountRepository.save(savedAccount))))
-                    .contentType(MediaType.APPLICATION_JSON))
-                .andExpect(status().isOk());
+        var json = mockMvc.perform(post("/api/persons/{personId}/accounts", "1098")
+                        .content(asJsonString(requestAccount))
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andReturn()
+                .getResponse()
+                .getContentAsString();
 
+        var uid = objectMapper.readValue(json, AccountDto.class).uid();
         Optional<Account> createdAccount = accountRepository.findAll().stream()
                 .filter(account -> account.getUid().equals(uid))
                 .findFirst();
 
         assertTrue(createdAccount.isPresent());
-        assertEquals((mapAccount(savedAccount).iban()), createdAccount.get().getIban());
+        assertEquals(requestAccount.iban(), createdAccount.get().getIban());
 
     }
 
     @Test
-    void shouldGetAllAccounts() throws Exception {
-
-        mockMvc.perform(get("/api/accounts"))
-                .andExpect(status().isOk())
-                .andExpect(jsonPath("$", hasSize(6)));
-    }
-
-    @Test
-    void shouldGetAccountsByPerson() throws Exception {
-
-        mockMvc.perform(get("/api/persons/{personId}/accounts", "1098"))
-                .andExpect(status().isOk())
-                .andExpect(jsonPath("$", hasSize(4)));
-    }
-
-
-    @Test
+    @DirtiesContext
     void updateAccount() throws Exception {
 
-        var uid = "uuid5";
-        var account = accountRepository.findAccountByUid(uid).orElseThrow();
-        var balance = 11000.00;
-        account.setBalance(balance);
-        mockMvc.perform(put("/api/persons/{personId}/accounts/{accountId}", "1048", uid)
-                .content(asJsonString(mapAccount(account)))
+        var person1 = personRepository.save(Person.builder()
+                .name("Anna Lex")
+                .email("annalex@gmail.com")
+                .uid("9654")
+                .build());
+
+        var person2 = personRepository.save(Person.builder()
+                .name("Tim Tent")
+                .email("timtent@gmail.com")
+                .uid("1098")
+                .build());
+
+        var person3 = personRepository.save(Person.builder()
+                .name("Joe More")
+                .email("joemorex@gmail.com")
+                .uid("2345")
+                .build());
+
+        accountRepository.save(Account.builder()
+                .uid("uuid1")
+                .iban("UA000000000000000000000123456789")
+                .balance(4500.00)
+                .person(person1)
+                .build());
+        accountRepository.save(Account.builder()
+                .uid("uuid2")
+                .iban("UA000000000000000000000987654321")
+                .balance(2700.00)
+                .person(person2)
+                .build());
+        accountRepository.save(Account.builder()
+                .uid("uuid3")
+                .iban("UA000000000000000000000918273645")
+                .balance(9000.00)
+                .person(person3)
+                .build());
+        accountRepository.save(Account.builder()
+                .uid("uuid4")
+                .iban("UA000000000000000000000123456789")
+                .balance(12000.00)
+                .person(person2)
+                .build());
+
+        var uid = "uuid4";
+        mockMvc.perform(put("/api/persons/{personId}/accounts/{accountId}", "1098", uid)
+                .content(asJsonString(AccountDto.builder()
+                        .iban("UA000000000000000000000123456789")
+                        .balance(11000.00)
+                        .build()))
                 .contentType(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk());
 
-        Optional<Account> updatedAccount = accountRepository.findAll().stream()
-                        .filter(account1 -> account1.getUid().equals(uid))
-                        .findFirst();
-
-        assertEquals(balance, updatedAccount.get().getBalance());
+        Optional<Account> updatedAccount = accountRepository.findAccountByUid(uid);
+        assertEquals(11000.00, updatedAccount.get().getBalance());
     }
 
     @Test
+    @DirtiesContext
     void deleteAccount() throws Exception {
 
-        var uid = "uuid5";
-        mockMvc.perform(delete("/api/persons/{personId}/accounts/{accountId}", "1048", uid))
+        personRepository.save(Person.builder()
+                .name("Anna Lex")
+                .email("annalex@gmail.com")
+                .uid("9654")
+                .build());
+
+        personRepository.save(Person.builder()
+                .name("Tim Tent")
+                .email("timtent@gmail.com")
+                .uid("1098")
+                .build());
+
+        personRepository.save(Person.builder()
+                .name("Joe More")
+                .email("joemorex@gmail.com")
+                .uid("2345")
+                .build());
+
+        accountRepository.save(Account.builder()
+                .uid("uuid1")
+                .iban("UA000000000000000000000123456789")
+                .balance(4500.00)
+                .person(personRepository.getReferenceById(1L))
+                .build());
+        accountRepository.save(Account.builder()
+                .uid("uuid2")
+                .iban("UA000000000000000000000987654321")
+                .balance(2700.00)
+                .person(personRepository.getReferenceById(2L))
+                .build());
+        accountRepository.save(Account.builder()
+                .uid("uuid3")
+                .iban("UA000000000000000000000918273645")
+                .balance(9000.00)
+                .person(personRepository.getReferenceById(3L))
+                .build());
+        accountRepository.save(Account.builder()
+                .uid("uuid4")
+                .iban("UA000000000000000000000123456789")
+                .balance(12000.00)
+                .person(personRepository.getReferenceById(2L))
+                .build());
+
+        var uid = "uuid3";
+        mockMvc.perform(delete("/api/persons/{personId}/accounts/{accountId}", "2345", uid))
                 .andExpect(status().isOk());
 
         assertFalse(accountRepository.findAccountByUid(uid).isPresent());
     }
 
-    @Test
-    void contextStarting(){
-
-    }
-
-    public static String asJsonString(final  Object obj) {
+    public String asJsonString(final Object obj) {
         try {
-            return new ObjectMapper().writeValueAsString(obj);
+            return objectMapper.writeValueAsString(obj);
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
-    }
-
-    private AccountDto mapAccount(Account account) {
-        return AccountDto.builder()
-                .uid(account.getUid())
-                .iban(account.getIban())
-                .balance(account.getBalance())
-                .person_id(account.getPerson().getId())
-                .build();
     }
 }
