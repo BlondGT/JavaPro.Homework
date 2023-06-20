24debca2102523cfd44aab796b1b3004f507567a
Tatiana <112488079+BlondGT@users.noreply.github.com>
2023-06-20 21:35:34 +0300

    Integration Test with outside API
    * Added hm32 (Integration Test with outside API)
    
    * Fixed hm32 (Integration Test with outside API)
    
    * Changed properties hm32 (Integration Test with outside API)

---
+++ src/test/java/bank/hm31/CurrencyControllerIntegrationTest.java
@@ -0,0 +1,83 @@
+package bank.hm31;
+
+import bank.hm31.response.CurrencyApiResponse;
+import bank.hm31.response.CurrencyApiResponseMeta;
+import bank.hm31.response.ResponseCode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.github.tomakehurst.wiremock.WireMockServer;
+import com.github.tomakehurst.wiremock.client.WireMock;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import java.time.Instant;
+import java.util.Map;
+
+import static com.github.tomakehurst.wiremock.client.WireMock.aResponse;
+import static com.github.tomakehurst.wiremock.client.WireMock.equalTo;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@ActiveProfiles("test")
+class CurrencyControllerIntegrationTest {
+
+    @Autowired
+    protected WireMockServer wireMockServer;
+
+    @Autowired
+    protected MockMvc mockMvc;
+
+    @Autowired
+    protected ObjectMapper objectMapper;
+
+    @DynamicPropertySource
+    public static void registerWiremockBaseUrl(DynamicPropertyRegistry registry) {
+
+        registry.add("wiremock.baseurl", WiremockConfig.wireMockServer::baseUrl);
+    }
+
+    @Test
+    void shouldGetCurrencyConverter() throws Exception {
+
+        var body = CurrencyApiResponse.builder()
+                .meta(CurrencyApiResponseMeta.builder()
+                        .lastTimeUpdated(Instant.now())
+                        .build())
+                .data(Map.of("EUR", ResponseCode.builder()
+                        .code("EUR")
+                        .value(0.25)
+                        .build()))
+                .build();
+
+        wireMockServer.stubFor(WireMock.get(WireMock.urlPathEqualTo("/apiCurrency"))
+                .withQueryParam("apikey", equalTo("12345"))
+                .withQueryParam("base_currency", equalTo("UAH"))
+                .withQueryParam("currencies", equalTo("EUR"))
+                .willReturn(aResponse()
+                        .withHeader("Content-Type", "application/json")
+                        .withBody(objectMapper.writeValueAsString(body))));
+
+        var query = MockMvcRequestBuilders.get("/currencies/convert")
+                .param("from", "UAH")
+                .param("to", "EUR")
+                .param("amount", "100");
+
+        var response = mockMvc.perform(query)
+                .andExpect(status().isOk())
+                .andReturn()
+                .getResponse()
+                .getContentAsString();
+
+        var result = Double.parseDouble(response);
+
+        assertEquals(25.0, result);
+    }
+}
